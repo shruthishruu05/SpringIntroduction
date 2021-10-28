79872466ede11fe70a3ae161cca0f33507607fee
shruthishruu05 <shruthi.m@perfios.com>
2021-10-28 20:31:04 +0530

    ADDED : Ability to create helloworld spring app to display welcome to bridgelabz

--- src/main/java/com/example/demo/controller/HelloWebController.java
+++ src/main/java/com/example/demo/controller/HelloWebController.java
@@ -1,5 +1,20 @@
 package com.example.demo.controller;
 
-public class HelloWebController {
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
+public class HelloWebController {
+	@GetMapping("/web")
+	public String hello() {
+		return "hello";
+	}
+	@GetMapping("/web/message")
+	public String message(Model model) {
+		model.addAttribute("message", "this is a custom message. Hello from Bridgelabz");
+		
+		return "message";
+		
+	}
 }
