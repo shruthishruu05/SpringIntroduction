541d48e6b143a65c89ab6ce9900b6863252c7e7c
shruthishruu05 <shruthi.m@perfios.com>
2021-10-29 19:58:21 +0530

    ADDED : Ability to make use of loggers

--- src/main/java/com/example/demo/HelloWorldDemoApplication.java
+++ src/main/java/com/example/demo/HelloWorldDemoApplication.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -9,13 +11,13 @@ import com.example.demo.component.DemoBean;
 
 @SpringBootApplication
 public class HelloWorldDemoApplication {
-
+	public static Logger logger = LoggerFactory.getLogger(HelloWorldDemoApplication.class);
 	public static void main(String[] args) {
 		System.out.println("hii");
 		ApplicationContext context = SpringApplication.run(HelloWorldDemoApplication.class, args);
 		
 		DemoBean demoBean = context.getBean(DemoBean.class);
-		System.out.println(demoBean.toString());
+		logger.debug(demoBean.toString());
 	
 	}
 
