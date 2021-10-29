ed640571ea7483cdab4d673841cb802a1079c9ee
shruthishruu05 <shruthi.m@perfios.com>
2021-10-29 19:09:39 +0530

    ADDED : Ability to create bean

--- src/main/java/com/example/demo/HelloWorldDemoApplication.java
+++ src/main/java/com/example/demo/HelloWorldDemoApplication.java
@@ -2,12 +2,21 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.example.demo.component.DemoBean;
+
 
 @SpringBootApplication
 public class HelloWorldDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HelloWorldDemoApplication.class, args);
+		System.out.println("hii");
+		ApplicationContext context = SpringApplication.run(HelloWorldDemoApplication.class, args);
+		
+		DemoBean demoBean = context.getBean(DemoBean.class);
+		System.out.println(demoBean.toString());
+	
 	}
 
 }
